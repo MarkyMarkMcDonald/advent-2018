2c99396961148ff96d8265699168f0a52d69dcc3
Mark McDonald <mark.mcdonald.ga@gmail.com>
2018-12-01 15:29:06 -0500

    Day 1 Part 2

--- src/main/kotlin/do_something.kt
+++ src/main/kotlin/do_something.kt
@@ -3,6 +3,29 @@ import java.io.File
 fun main(args: Array<String>) {
     val lines = File("src/main/resources/input.txt").inputStream().bufferedReader().readLines()
     val numbers = lines.map(Integer::parseInt)
-    val result = numbers.sum()
-    println(result)
+
+    var currentFrequency = 0
+    val pastFrequencies = mutableSetOf(currentFrequency)
+    var repeatedFrequency: Int? = null
+    var index = 0
+
+    while (repeatedFrequency == null) {
+        if (index >= numbers.size) {
+            index = 0
+        }
+
+        val number = numbers[index]
+
+        val nextFrequency = currentFrequency + number
+        val added = pastFrequencies.add(nextFrequency)
+        currentFrequency = nextFrequency
+
+        if (!added) {
+            repeatedFrequency = nextFrequency
+        }
+
+        index += 1
+    }
+
+    println(repeatedFrequency)
 }
