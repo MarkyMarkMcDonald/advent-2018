79fe314aaf02e6a684cb226cc076b2366ec9aac1
Mark McDonald <mark.mcdonald.ga@gmail.com>
2018-12-05 01:28:17 -0500

    Day 5 part 2

--- src/main/kotlin/do_something.kt
+++ src/main/kotlin/do_something.kt
@@ -1,27 +1,41 @@
-fun main(args: Array<String>) {
-    var chars = input().first().toCharArray().toList()
+import java.util.stream.Collectors
 
-    while (chars.isReducable()) {
-        chars = chars.reducify()
+fun main(args: Array<String>) {
+    val chars = input().first().toCharArray().toList()
+
+    val withoutWorries = ('a'..'z').map { char ->
+        chars.filterNot { it.equals(char, ignoreCase = true) }
     }
 
-    println(chars.size)
+    val furtherReductions = withoutWorries.
+        parallelStream().
+        map(::fullyReduced).
+        map(List<*>::size).
+        collect(Collectors.toList())
+
+    val lowestLength = furtherReductions.minBy { it }
+
+    println(lowestLength)
+}
+
+private fun fullyReduced(chars: List<Char>): List<Char> {
+    var chars1 = chars
+    while (chars1.isReducable()) {
+        chars1 = chars1.reducify()
+    }
+    return chars1
 }
 
 private fun List<Char>.reducify(): List<Char> {
     val reaction = this.reactors().indexOfFirst(::willReact)
-    return toList().subList(0, reaction).plus(toList().subList(reaction + 2, size))
+    return subList(0, reaction).plus(subList(reaction + 2, size))
 }
 
 private fun List<Char>.isReducable(): Boolean = reactors().any(::willReact)
 
-private fun List<Char>.reactors() = this.toList().windowed(2)
+private fun List<Char>.reactors() = this.windowed(2)
 
 private fun willReact(it: List<Char>): Boolean {
-    if (it.size != 2) {
-        throw IllegalArgumentException()
-    }
-
     val first = it[0]
     val second = it[1]
 
