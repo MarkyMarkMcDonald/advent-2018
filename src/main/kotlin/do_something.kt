406eca58b39a2d029a402f4626d955bf3d48134c
Mark McDonald <mark.mcdonald.ga@gmail.com>
2018-12-08 16:50:18 -0500

    Day 6 part 2

--- src/main/kotlin/do_something.kt
+++ src/main/kotlin/do_something.kt
@@ -1,48 +1,20 @@
 fun main(args: Array<String>) {
     val pointsOfInterest: List<PointOfInterest> = input().map(::parsePointOfInterest)
 
-    val theWholeWorld = (0..400).map { x ->
-        (0..400).map { y ->
-            val minimums = pointsOfInterest.minimums(x, y)
-            val closest = if (minimums.size == 1) {
-                minimums.first()
-            } else {
-                null
-            }
-            Coordinate(x, y, closest)
+    var count = 0
+
+    (-32..464).forEach { x ->
+        (-32..464).forEach { y ->
+            val distanceSums = pointsOfInterest.distancesSum(x, y)
+            if (distanceSums < 10000) count += 1
         }
     }
 
-    val northPole = (0..400).map { theWholeWorld[it][0] }
-    val southPole = (0..400).map { theWholeWorld[it][400] }
-    val northAmerica = (0..400).map { theWholeWorld[0][it] }
-    val australia = (0..400).map { theWholeWorld[400][it] }
-
-    val toInfinities: List<PointOfInterest> =
-        (northPole + southPole + northAmerica + australia).
-            mapNotNull(Coordinate::closestPointOfInterest)
-
-    val theKnownWorld = theWholeWorld.flatten().filterNot { toInfinities.contains(it.closestPointOfInterest) }
-
-    val asia: Int = theKnownWorld.filter { it.closestPointOfInterest != null }.groupBy(Coordinate::closestPointOfInterest).values.maxBy { it.size }!!.size
-
-    println(asia)
+    println(count)
 }
 
-private fun List<PointOfInterest>.minimums(x: Int, y: Int): List<PointOfInterest> {
-    var minimums: List<PointOfInterest> = mutableListOf()
-    var minValue = Integer.MAX_VALUE
-
-    for (point in this) {
-        val distance = point.manhattanDistance(x, y)
-        if (distance == minValue) {
-            minimums += point
-        } else if (distance < minValue) {
-            minValue = distance
-            minimums = mutableListOf(point)
-        }
-    }
-    return minimums
+private fun List<PointOfInterest>.distancesSum(x: Int, y: Int): Int {
+    return this.sumBy { it.manhattanDistance(x, y) }
 }
 
 fun parsePointOfInterest(line: String): PointOfInterest {
@@ -50,8 +22,6 @@ fun parsePointOfInterest(line: String): PointOfInterest {
     val y = line.split(", ")[1].toInt()
     return PointOfInterest(x, y)
 }
-
-data class Coordinate(val x: Int, val y: Int, val closestPointOfInterest: PointOfInterest?)
 
 data class PointOfInterest(val x: Int, val y: Int) {
     fun manhattanDistance(x2: Int, y2: Int): Int {
