494bfea9ba190a6ee0b49fd0e3ebd1b147b407a5
Mark McDonald <mark.mcdonald.ga@gmail.com>
2018-12-16 14:50:28 -0500

    Working on day 7 part 2

--- src/main/kotlin/do_something.kt
+++ src/main/kotlin/do_something.kt
@@ -1,3 +1,6 @@
+val STEP_OVERHEAD = 60
+val NUMBER_OF_WORKERS = 5
+
 fun main(args: Array<String>) {
     val instructions: List<Instruction> = input().map(::parseInstruction)
 
@@ -8,51 +11,109 @@ fun main(args: Array<String>) {
 
     val startingInstructions = instructions
         .filter { startingPoints.contains(it.step) }
-        .map { Instruction('*', it.step) }
+        .map { Instruction('*', it.step) }.distinct()
         .sortedBy { it.nextStep }
 
-    val orderedInstructions = orderedInstructions(startingInstructions, instructions)
+    val (_, time) = orderedInstructions(listOf(), mapOf(), instructions + startingInstructions, 0, WorkerPool(NUMBER_OF_WORKERS))
 
-    val path = walkThisWay(orderedInstructions)
-
-    println(path)
+    println(time)
 }
+
+val COSTS: Map<Char, Int> = ('A'..'Z').mapIndexed { index, char -> Pair(char, index + 1 + STEP_OVERHEAD) }.toMap()
+
+class WorkerPool(max: Int) {
+    private var count = max
+
+    fun availability() : Int {
+        return count
+    }
+
+    fun take(number: Int){
+        count -= number
+        if (count < 0) throw IllegalArgumentException("you took too many")
+    }
+
+    fun giveBack(number: Int){
+        count += number
+    }
+}
+
+typealias SecondsRemaining = Int
+typealias Seconds = Int
+typealias InstructionsAndCost = Pair<List<Instruction>, Seconds>
 
 tailrec fun orderedInstructions(
     visitedInstructions: List<Instruction>,
-    remainingInstructions: List<Instruction>
-): List<Instruction> {
+    inProduction: Map<SecondsRemaining, Instruction>,
+    remainingInstructions: List<Instruction>,
+    timeSoFar: Seconds,
+    workerPool: WorkerPool
+): InstructionsAndCost {
     return if (remainingInstructions.isEmpty()) {
-        visitedInstructions
+        Pair(visitedInstructions + inProduction.map { it.value }, timeSoFar + (inProduction.keys.max() ?: 0))
     } else {
-        val possibleNextInstructions = remainingInstructions.filter { instruction ->
-            val comesNext = isNext(visitedInstructions, instruction)
-            val blocked = (remainingInstructions).any {
-                val samePath = it.nextStep == instruction.nextStep
-                val notNext = !isNext(visitedInstructions, it)
-                samePath && notNext
-            }
-            comesNext && !blocked
+        val possibleNextInstructions = workToBePickedUp(remainingInstructions, visitedInstructions)
+        val noWorkersAvailable = workerPool.availability() == 0
+        val noWorkToBeDone = possibleNextInstructions.isEmpty()
+
+        if (noWorkersAvailable || noWorkToBeDone) {
+            // simulate time passing and move WIP to visited
+            val minimumTimeLeft = inProduction.keys.min()!!
+            val finishedActions: Map<SecondsRemaining, Instruction> = inProduction.filter { it.key == minimumTimeLeft }
+            val newTimeSoFar = timeSoFar + minimumTimeLeft
+            val newInProduction: Map<SecondsRemaining, Instruction> = inProduction.minus(finishedActions.map { it.key }).mapKeys { it.key - minimumTimeLeft }
+            val newVisitedInstructions = visitedInstructions + finishedActions.values
+            workerPool.giveBack(finishedActions.size)
+            orderedInstructions(
+                newVisitedInstructions, newInProduction, remainingInstructions, newTimeSoFar, workerPool
+            )
+
+        } else {
+            // Move as much unblocked work as we have workers for from remaining to inProduction
+            val availableWorkers = workerPool.availability()
+            val accomplishableWork = possibleNextInstructions.take(availableWorkers)
+            workerPool.take(accomplishableWork.size)
+
+            val newInProduction: Map<SecondsRemaining, Instruction> = inProduction.plus(accomplishableWork.map { Pair(COSTS[it.nextStep]!!, it) })
+
+            orderedInstructions(
+                visitedInstructions,
+                newInProduction,
+                remainingInstructions.filter {
+                    val targetNextSteps = accomplishableWork.map { a -> a.nextStep }
+                    !targetNextSteps.contains(it.nextStep)
+                },
+                timeSoFar,
+                workerPool
+            )
         }
-        val nextInstruction: Instruction = possibleNextInstructions.minBy { it.nextStep }!!
-        orderedInstructions(visitedInstructions + nextInstruction, remainingInstructions - nextInstruction)
     }
+}
+
+private fun workToBePickedUp(
+    remainingInstructions: List<Instruction>,
+    visitedInstructions: List<Instruction>
+): List<Instruction> {
+    val specialFirstCases = remainingInstructions.filter { it.step == '*' }
+    if (specialFirstCases.isNotEmpty()) {
+        return specialFirstCases.sortedBy { it.nextStep }
+    }
+
+    return (remainingInstructions).filter { instruction ->
+        val comesNext = isNext(visitedInstructions, instruction)
+        val blocked = (remainingInstructions).any {
+            val samePath = it.nextStep == instruction.nextStep
+            val notNext = !isNext(visitedInstructions, it)
+            samePath && notNext
+        }
+        comesNext && !blocked
+    }.sortedBy { it.nextStep }
 }
 
 private fun isNext(
     visitedInstructions: List<Instruction>,
     instruction: Instruction
 ) = visitedInstructions.map(Instruction::nextStep).contains(instruction.step)
-
-private fun walkThisWay(orderedInstructions: List<Instruction>): String {
-    var path = ""
-    orderedInstructions.map(Instruction::nextStep).forEach {
-        if (!path.contains(it)) {
-            path += it
-        }
-    }
-    return path
-}
 
 data class Instruction(val step: Char, val nextStep: Char)
 
